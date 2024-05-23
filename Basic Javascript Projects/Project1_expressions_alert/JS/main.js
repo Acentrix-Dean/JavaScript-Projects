daa8dc35e687ba41871a64f19924f3e6ceb68c3e
Acentrix-Dean <deanblackburn@hotmail.co.uk>
2024-05-23 15:32:22 +0100

    Update main.js

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,4 @@
 var sentOne = 'This is the beginning of the sentence,', sentTwo = ' and this is the end of the sentence.' //Setting the values of two new variables
 var popup = 'I said \"This is an alert window!\"'; //Setting the value of a new variable
 window.alert(popup); //Calling an alert window with the value stored inside the 'popup' variable
-document.write(text + textTwo); //Taking the sentOne and sentTwo variables, concatenating them and printing them to the document
+document.write(sentOne + sentTwo); //Taking the sentOne and sentTwo variables, concatenating them and printing them to the document
